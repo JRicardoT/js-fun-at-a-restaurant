f9be1e03227ed1a4ee7a77a991f666b25c4e2d04
jose Tovar <jose.tovar102397@gmail.com>
2021-08-28 12:19:36 -0600

    Add meal

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,46 @@
-function nameMenuItem() {
+function nameMenuItem(menuItemName) {
+  return `Delicious ${menuItemName}`;
+};
 
-}
+function createMenuItem(menuItemName, price, type) {
+  var menuItem = {
+    name: menuItemName,
+    price: price,
+    type: type
+  }
+  return menuItem;
+};
 
+function addIngredients(topping, ingredients) {
+  if (ingredients.includes(topping)) {
+    return topping
+  } else {
+    return ingredients.push(topping);
+  }
+};
+
+function formatPrice(initialPrice) {
+  return "$" + initialPrice;
+};
+
+function decreasePrice(decreasedPrice) {
+  return decreasedPrice - (decreasedPrice * .10);
+};
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+  return recipe;
+};
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
