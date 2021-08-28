c06df7efa668b405b0a8bf2df8d400e88443051b
jose Tovar <jose.tovar102397@gmail.com>
2021-08-28 13:16:18 -0600

    Add Order

--- src/order.js
+++ src/order.js
@@ -1,7 +1,39 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+    return deliveryOrders.push(order);
+  }
+};
+
+function refundOrder(refundOrderNumber, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+  if (deliveryOrders[i].orderNumber === refundOrderNumber) {
+    deliveryOrders.splice([i],1)
+    return deliveryOrders;
+  }
+ }
+};
+
+function listItems(deliveryOrders) {
+  items = [];
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    items.push(deliveryOrders[i].item);
+  }
+  return items.join(", ");
+};
+
+function searchOrder(deliveryOrders, item) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item.includes(item)) {
+      return true;
+    }
+  }
+  return false;
+}
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+   takeOrder,
+   refundOrder,
+   listItems,
+   searchOrder
 }
