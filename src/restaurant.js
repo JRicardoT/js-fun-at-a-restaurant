7a4e89a4aae8033681902ea7323b7c4677e3dafe
jose Tovar <jose.tovar102397@gmail.com>
2021-08-29 18:36:46 -0600

    Add restaurant iteration

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,43 @@
+function createRestaurant(name) {
+  pizzaRestaurant = {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    }
+  }
+  return pizzaRestaurant;
+};
+
+ function addMenuItem(pizzaRestaurant, item) {
+    if (pizzaRestaurant.menus.breakfast.includes(item)) {
+      return;
+    } else if (item.type === "breakfast") {
+      return pizzaRestaurant.menus.breakfast.push(item);
+    } else if (item.type === "lunch") {
+      return pizzaRestaurant.menus.lunch.push(item);
+    } else {
+      return pizzaRestaurant.menus.dinner.push(item);
+    }
+ };
+
+  function removeMenuItem (pizzaRestaurant, itemName, itemType) {
+    for (var i = 0; i < pizzaRestaurant.menus[itemType].length; i++) {
+      if (pizzaRestaurant.menus[itemType][i].name === itemName) {
+      pizzaRestaurant.menus[itemType].splice(i,1)
+      return `No one is eating our ${itemName} - it has been removed from the ${itemType} menu!`
+    }
+   }
+   return `Sorry, we don't sell ${itemName}, try adding a new recipe!`
+  };
+
+
+
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+   createRestaurant,
+   addMenuItem,
+   removeMenuItem
 }
